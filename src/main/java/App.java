6c52355273ce0772122609dad76b11d9086a45d8
Egor Pushka <pushkartegor@gmail.com>
2023-09-11 00:25:58 +0300

    fix:1 mentors comments corrected

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,12 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
 
-        Cat cat1 = applicationContext.getBean("cat", Cat.class);
-        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean1 == bean2);
+
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
 
         System.out.println(cat1 == cat2);
 
